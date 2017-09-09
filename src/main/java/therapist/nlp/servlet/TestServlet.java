3ed78da6dfaf50be75662faf8b714f02f3c09234
Varun Radhakrishnan <vradhak2@jhu.edu>
2017-09-08 23:53:38 -0400

    fixed some bugs with the Test Servlet

--- src/main/java/therapist/nlp/servlet/TestServlet.java
+++ src/main/java/therapist/nlp/servlet/TestServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 import java.io.PrintWriter;
+import java.io.IOException;
 
 public class TestServlet extends HttpServlet {
 
@@ -15,7 +16,11 @@ public class TestServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
 	response.setContentType("text/html");
-	PrintWriter out = response.getWriter();
-	out.println("<h1>This shit works</h1>");
+
+	try (PrintWriter out = response.getWriter()) {
+	    out.println("<h1>This shit works</h1>");
+	} catch (IOException e) {
+
+	}
     }
 }
