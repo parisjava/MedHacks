d83ea1171123b0fe842310821f249a054762a24f
Varun Radhakrishnan <vradhak2@jhu.edu>
2017-09-08 23:45:11 -0400

    added test servlet

---
+++ src/main/java/therapist/nlp/servlet/TestServlet.java
@@ -0,0 +1,11 @@
+public class TestServlet extends HttpServlet {
+
+    public void init() throws ServletException {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	response.setContentType("text/html");
+	PrintWriter out = response.getWriter();
+	out.println("<h1>This shit works</h1>");
+    }
+}
