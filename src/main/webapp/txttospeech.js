d4a7235d763784df6087ea5704f30dd6f8a754b7
Varun Radhakrishnan <vradhak2@jhu.edu>
2017-09-10 01:38:42 -0400

    deleted some files

--- src/main/webapp/txttospeech.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-function voiceInputOver(val){
-     alert("Voice input is complete");
-     alert("Your input is " + val);
-}
-</script>
-<input onwebkitspeechchange="voiceInputOver(this.value)" x-webkit-speech />
